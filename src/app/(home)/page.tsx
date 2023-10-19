1789bb90119386445d5d5562c3bc1aad63d14791
Guilherme <borimdev@gmail.com>
2023-10-19 14:02:55 -0300

    feat: add mouses and headphones to homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -20,6 +20,13 @@ export default async function Home() {
       },
     },
   });
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
   return (
     <div className="flex flex-col gap-8 py-8">
       <PromoBanner src="/banner-home-01.png" alt="50% de desconto" />
@@ -38,6 +45,13 @@ export default async function Home() {
       <div>
         <SectionTile>Teclados</SectionTile>
         <ProductList products={keyboards} />
+      </div>
+
+      <PromoBanner src="/banner-home-03.png" alt="50% de desconto em mouses" />
+
+      <div>
+        <SectionTile>Mouses</SectionTile>
+        <ProductList products={mouses} />
       </div>
     </div>
   );
