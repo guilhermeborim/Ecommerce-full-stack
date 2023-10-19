80a8e1d98509a70e58ae185744d379aff615bead
Guilherme <borimdev@gmail.com>
2023-10-19 10:59:01 -0300

    feat: add ProductItem and ProductList components

---
+++ src/app/(home)/components/product-list.tsx
@@ -0,0 +1,21 @@
+import ProductItem from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/products";
+import { Product } from "@prisma/client";
+
+interface productListProps {
+  products: Product[];
+}
+
+const ProductList = ({ products }: productListProps) => {
+  return (
+    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+      {products.map((product) => (
+        <ProductItem
+          key={product.id}
+          product={computeProductTotalPrice(product)}
+        />
+      ))}
+    </div>
+  );
+};
+export default ProductList;
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,33 @@
-"use client";
-
 import Image from "next/image";
 import Categories from "./components/categories";
-export default function Home() {
+import { prismaClient } from "@/lib/prisma";
+import ProductList from "./components/product-list";
+
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
   return (
-    <div className="p-5">
+    <div>
       <Image
         src="/banner-home-01.png"
         alt="50% de desconto"
         width={0}
         height={0}
-        className="h-auto w-full"
+        className="h-auto w-full px-5"
         sizes="100vw"
       />
       <div className="mt-8">
         <Categories />
+      </div>
+
+      <div className="mt-8">
+        <ProductList products={deals} />
       </div>
     </div>
   );
